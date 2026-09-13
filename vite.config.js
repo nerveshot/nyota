d2e0318a1aa773ba2fd182e95f3ac60e5b4860dc
nerveshot <faizansalam@icloud.com>
2026-09-13 12:55:43 +0530

    Fix UI asset relative paths and remove gitignore

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Ensures relative assets so it works on GitHub Pages, subpaths, and all web hosts
 })
